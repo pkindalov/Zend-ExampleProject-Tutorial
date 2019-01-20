20c4749bb15780b33ef778af8892b41b21fc40c5
pkindalov <panayotkindalov@gmail.com>
2019-01-20 21:54:55 +0200

    Added new files - WriteController, WriteControllerFactory, ZendDbSqlCommandFactory, PostFieldset, PostForm, PostCommand, ZendDbSqlCommand and add.phtml. Updated file module.config.php from directory module/Blog/config

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -10,16 +10,20 @@ return [
     'service_manager' => [
         'aliases' => [
             Model\PostRepositoryInterface::class => Model\ZendDbSqlRepository::class,
+            Model\PostCommandInterface::class => Model\ZendDbSqlCommand::class,
         ],
         'factories' => [
             Model\PostRepository::class => InvokableFactory::class,
             Model\ZendDbSqlRepository::class => Factory\ZendDbSqlRepositoryFactory::class,
+            Model\PostCommand::class => InvokableFactory::class,
+            Model\ZendDbSqlCommand::class => Factory\ZendDbSqlCommandFactory::class,
         ],
     ],
     'controllers' => [
                         'factories' => [
 //                            Controller\ListController::class => InvokableFactory::class,
                                Controller\ListController::class => Factory\ListControllerFactory::class,
+                               Controller\WriteController::class => Factory\WriteControllerFactory::class,
                         ],
 
                     ],
@@ -49,8 +53,24 @@ return [
                             ],
                         ],
                     ],
+                    
+                    'add' => [
+                        'type' => Literal::class,
+                        'options' => [
+                        'route' => '/add',
+                        'defaults' => [
+                        'controller' => Controller\WriteController::class,
+                        'action' => 'add'
+                        ],
+                    ],
+                 ],
+               
                 ],
             ],
+            
+ 
+            
+            
         ],
     ],
 
